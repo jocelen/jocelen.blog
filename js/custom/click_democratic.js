10e46cd9704c4dc2140cba54085ad885bb92592f
jocelen <jocelen@163.com>
2018-10-25 16:47:50 +0800

    Site updated: 2018-10-25 16:47:48

--- js/custom/click_democratic.js
+++ js/custom/click_democratic.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function($) {
         var x = e.pageX, 
         y = e.pageY; 
         $i.css({ 
-            "z-index": 99999999999999, 
+            "z-index": 2147483647, 
             "top": y - 20, 
             "left": x, 
             "position": "absolute", 
